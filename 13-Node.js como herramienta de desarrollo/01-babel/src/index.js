6f795921776a22904bcff390c95cec42668f4cf7
“ccinetto” <cristiancinetto@gmail.com>
2022-04-16 13:16:03 -0300

    prueba

--- 13-Node.js como herramienta de desarrollo/01-babel/src/index.js
+++ 13-Node.js como herramienta de desarrollo/01-babel/src/index.js
@@ -1,7 +1,6 @@
 import Color from './colors';
 
-for(let i =0; i<200; i++){
-    const miColor = new Color();
-    console.log(`Pedido ${i}: ${JSON.stringify(miColor.color)}`)
-    
+for (let i = 0; i < 200; i++) {
+  const miColor = new Color();
+  console.log(`pedido ${i}: ${JSON.stringify(miColor.color)}`);
 }

6e36a874078c74d99b745efdd6ca8740de1f99d7
“ccinetto” <cristiancinetto@gmail.com>
2022-06-21 16:51:39 -0300

    clase33

---
+++ 33-Heroku/01-herokuapp/src/services/server.ts
@@ -0,0 +1,37 @@
+import express from 'express';
+import { PORT } from '..';
+import { isPrime } from '../utils';
+const app = express();
+
+app.get('/', (req, res) => {
+  console.log('Resolving / endpoint');
+  res.json({
+    pid: process.pid,
+    msg: `HOLA desde puerto ${PORT} y process id ${process.pid}`,
+  });
+});
+
+app.get('/prime', (req, res) => {
+  const primes = [];
+  const max = Number(req.query.max) || 1000;
+  for (let i = 1; i <= max; i++) {
+    if (isPrime(i)) primes.push(i);
+  }
+  res.json(primes);
+});
+
+app.get('/saludar', (req, res) => {
+  const primes = [];
+  const max = Number(req.query.max) || 1000;
+  for (let i = 1; i <= max; i++) {
+    if (isPrime(i)) primes.push(i);
+  }
+
+  res.json({
+    pid: process.pid,
+    msg: `HOLA desde puerto ${PORT} y process id ${process.pid}`,
+    variable: process.env.ENV_EJEMPLO,
+  });
+});
+
+export default app;
---
+++ 33-Heroku/01-herokuapp/src/utils/index.ts
@@ -0,0 +1,14 @@
+export const isPrime = (num: number): boolean => {
+  if ([2, 3].indexOf(num) >= 0) return true;
+  else if ([2, 3].some((n) => num % n == 0)) return false;
+  else {
+    let i = 5,
+      w = 2;
+    while (i * i <= num) {
+      if (num % i == 0) return false;
+      i += w;
+      w = 6 - w;
+    }
+  }
+  return true;
+};
